467e8227e4794bb985aa30dd438149a7454bfd08
aditigoyal291 <goyaladiti2912@gmail.com>
2023-10-09 18:02:11 +0530

    Added links in footer and made it responsive

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import { LiaCopyrightSolid } from "react-icons/lia";
+import Link from "next/link";
 const Footer = () => {
   return (
     <div className="p-10">
       <div className="border-secondary/60 border-t-[1px] w-full flex px-4 flex-col  ">
         <div className="flex gap-10 mt-5 flex-col sm:flex-row">
           <div className="flex flex-col gap-5">
-            <h1 className="text-2xl text-primary font-semibold ">NEXUS</h1>
+            <Link href="/">
+              <h1 className="text-2xl text-primary font-semibold ">NEXUS</h1>
+            </Link>
             <p className="text-xs">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint
               odit provident et natus maiores corrupti sequi id ab ipsa soluta.
@@ -18,18 +21,30 @@ const Footer = () => {
               <div className="h-[16px] w-[2px] bg-primary"></div>
               <h2 className="font-semibold text-xxs">Links</h2>
             </div>
-            <p className="text-white/70 text-xxs">Home</p>
-            <p className="text-white/70 text-xxs">Get started</p>
-            <p className="text-white/70 text-xxs">Services</p>
+            <Link href="/">
+              <p className="text-white/70 text-xxs">Home</p>
+            </Link>
+            <Link href="/about">
+              <p className="text-white/70 text-xxs">About Us</p>
+            </Link>
+            <Link href="/events">
+              <p className="text-white/70 text-xxs">Events</p>
+            </Link>
           </div>
           <div className="flex flex-col gap-3">
             <div className="flex gap-2 items-center">
               <div className="h-[16px] w-[2px] bg-primary"></div>
-              <h2 className="font-semibold text-xxs">Others</h2>
+              <h2 className="font-semibold text-xxs">Projects</h2>
             </div>
-            <p className="text-white/70 text-xxs">Home</p>
-            <p className="text-white/70 text-xxs">Get started</p>
-            <p className="text-white/70 text-xxs">Services</p>
+            <Link href="/projects">
+              <p className="text-white/70 text-xxs">All Projects</p>
+            </Link>
+            <Link href="/projects/1">
+              <p className="text-white/70 text-xxs">Project 1</p>
+            </Link>
+            <Link href="/projects/2">
+              <p className="text-white/70 text-xxs">Project 2</p>
+            </Link>
           </div>
           <div className="flex flex-col gap-3">
             <div className="flex gap-2 items-center">
@@ -37,27 +52,33 @@ const Footer = () => {
               <h2 className="font-semibold text-xxs">Others</h2>
             </div>
             <p className="text-white/70 text-xxs">Follow us on Social Media</p>
-            <div className="flex gap-2">
+            <div className="flex gap-2  justify-between">
+              <Link href="https://www.instagram.com/nexus.pesu/">
+                <Image
+                  src="/images/icons/social-media/instagram.svg"
+                  width={20}
+                  height={20}
+                  alt="insta"
+                />
+              </Link>
+              <Link href="https://www.linkedin.com/company/nexus-pes/">
+                <Image
+                  src="/images/icons/social-media/linkedin.svg"
+                  width={20}
+                  height={20}
+                  alt="linkedin"
+                />
+              </Link>
+              <Link href="https://github.com/Nexus-PES/">
+                <Image
+                  src="/images/icons/social-media/github.svg"
+                  width={20}
+                  height={20}
+                  alt="github"
+                />
+              </Link>
               <Image
-                src="/images/icons/social-media/blue-fill-instagram.svg"
-                width={20}
-                height={20}
-                alt="insta"
-              />
-              <Image
-                src="/images/icons/social-media/blue-linkedin.svg"
-                width={20}
-                height={20}
-                alt="linkedin"
-              />
-              <Image
-                src="/images/icons/social-media/blue-reddit.svg"
-                width={20}
-                height={20}
-                alt="reddit"
-              />
-              <Image
-                src="/images/icons/social-media/blue-telegram.svg"
+                src="/images/icons/social-media/discord.svg"
                 width={20}
                 height={20}
                 alt="telegram"
@@ -66,7 +87,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="text-sm flex gap-2 font-semibold">
+      <div className="text-sm flex gap-2 font-semibold mt-6 text-white/60">
         <LiaCopyrightSolid className="text-lg" />
         <p>2023 Nexus. All rights Reserved</p>
       </div>

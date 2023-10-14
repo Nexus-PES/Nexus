7d6ffa066026515c25f7b6d5d624ec746ae54887
Anshul Dasyam <pesanshul@gmail.com>
2023-10-15 02:04:28 +0530

    Backend Changes

--- app/api/events/route.js
+++ app/api/events/route.js
@@ -2,6 +2,18 @@ import { NextResponse } from "next/server";
 import { activityData } from "../../../data";
 
 export function GET() {
-	return NextResponse.json({ activityData }, {status: 200});
+	return NextResponse.json({ activityData }, { status: 200 });
 }
 
+export async function POST(request) {
+	console.log("Inside Events Route \n")
+	const req = await request.json();
+	const { eventId } = req;
+	// console.log(eventId);
+	// console.log(activityData);
+
+	const eventDetail = activityData.find((event) => event.id == eventId);
+	// console.log(eventDetail)
+	console.log("Outside")
+	return NextResponse.json(eventDetail, { status: 200 });
+}
--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -4,3 +4,16 @@ import { projectData } from "../../../data";
 export function GET() {
 	return NextResponse.json({ projectData }, { status: 200 });
 }
+
+export async function POST(request) {
+	const req = await request.json();
+	const { projectId } = req;
+	// console.log(projectId);
+	console.log(projectId);
+
+	const projectDetail = projectData.find(
+		(project) => project.id == projectId
+	);
+	// console.log(projectDetail)
+	return NextResponse.json(projectDetail, { status: 200 });
+}
